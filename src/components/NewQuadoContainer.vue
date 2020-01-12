3051257b06bcccfbe58f39ed5c906f0d8c6d1437
math <mathilde.grapin@gmail.com>
2020-01-12 12:23:37 +0100

    :heavy_plus_sign: Add vuetify

---
+++ src/components/NewQuadoContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-col>
+      <v-row>
+        <v-form>
+          <v-text-field label="Category name" required> </v-text-field>
+        </v-form>
+      </v-row>
+    </v-col>
+  </v-container>
+</template>
+
+<script>
+export default {}
+</script>
---
+++ src/components/QuadoContainer.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-card>
+    <v-simple-table>
+      <tbody>
+        <tr>
+          <td>Revoir Johyn</td>
+          <td>Demain</td>
+        </tr>
+        <tr>
+          <td>Arroser les plantes</td>
+          <td>La semaine prochaine</td>
+        </tr>
+        <tr>
+          <td>Ecouter de la musique</td>
+          <td>En rentrant</td>
+        </tr>
+        <tr>
+          <td>Ono</td>
+          <td>Faire les conférences métiers</td>
+        </tr>
+      </tbody>
+    </v-simple-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'QuadoContainer'
+}
+</script>
